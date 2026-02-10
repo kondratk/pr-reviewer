4d2a065f31abb6666e6c33d3534c7977fdd5f510
Tomas Navickas <tomas.navickas@fresha.com>
2026-02-10 14:20:16 +0000

    Initial Commit

---
+++ pr_reviews/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pr_reviews
+//
+//  Created by Tomas Navickas on 10/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pr_reviews/pr_reviewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  pr_reviewsApp.swift
+//  pr_reviews
+//
+//  Created by Tomas Navickas on 10/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct pr_reviewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
